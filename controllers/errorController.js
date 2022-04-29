02b1f45892963b6143cc26e575df79aee1850711
allaoua-rico <allaoua.boudriou@gmail.com>
2022-04-29 01:52:45 +0100

    fixed start script

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,4 +1,4 @@
-const AppError = require("../backLib/appError.Js");
+const AppError = require("../backLib/appError.js");
 
 const handleCastErrorDB = (err) => {
   const message = `Invalid ${err.path}: ${err.value}.`;
