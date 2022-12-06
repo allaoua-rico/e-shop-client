95a82f1d2764815a29544bac2b056c1940d8f786
unknown <allaoua.boudriou@gmail.com>
2022-12-06 11:49:12 +0100

    First commit

--- next.config.js
+++ next.config.js
@@ -2,12 +2,12 @@
 
 module.exports = {
   reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: "http://localhost:4000/api/:path*", // Proxy to Backend
-      },
-    ];
-  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: "/api/:path*",
+  //       destination: "http://localhost:4000/api/:path*", // Proxy to Backend
+  //     },
+  //   ];
+  // },
 };
