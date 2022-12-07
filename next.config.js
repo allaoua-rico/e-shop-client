b709ef0c3757fe00430c4d3f990a5bed1b3debf5
unknown <allaoua.boudriou@gmail.com>
2022-12-07 14:16:53 +0100

    SERVER_URL ADDED

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ module.exports = {
     return [
       {
         source: "/api/:path*",
-        destination: `${process.env.SERVER_URL}/api/:path*`, // Proxy to Backend
+        destination: `${process.env.NEXT_PUBLIC_SERVER_URL}/api/:path*`, // Proxy to Backend
       },
     ];
   },
